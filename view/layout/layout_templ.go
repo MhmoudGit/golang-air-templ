f130d3de400a280a4309c909dc831702c2b569cc
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2024-01-03 00:00:16 +0200

    layout

--- view/layout/layout_templ.go
+++ view/layout/layout_templ.go
@@ -32,7 +32,7 @@ func Base() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title></head><body><h1 class=\"font-semibold\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title></head><body><h1 class=\"font-semibold text-cyan-600\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
